3bbfc73c783f119041c9a6cb4f71c866ea0c6c23
Padmaraj Bhat <padmarajbhat@gmail.com>
2022-06-30 18:53:41 +0530

    code optimization at word cloud

--- AngularFrontEnd/src/app/Components/wordcloud/wordcloud.component.ts
+++ AngularFrontEnd/src/app/Components/wordcloud/wordcloud.component.ts
@@ -42,35 +42,30 @@ export class WordcloudComponent implements OnInit {
 
   constructor(public commServ: CommonService) {
     this.gita_Json = this.commServ.gita_df;
-    let context = this.commServ.getWordCloudContext();
-    console.log(context);
-    if (!context) {
-      this.fetchD3DataArray(this.gita_Json);
-    } else {
-      this.data = context['data'];
-      this.chapters = context['chapters'];
-      this.uvacha = context.uvacha;
-    }
+    
   }
 
   ngOnInit() {
-    console.log(this.data)}
+    console.log("ngOnInit",this.data);
+    let context = this.commServ.getWordCloudContext();
+    console.log("Context : ", context);
+    if (context) {
+        this.chapters = context['chapters'];
+        this.uvacha = context.uvacha;
+    }
+    this.reloadData();
+  }
 
   getUvachaList() {
-    //console.log('what is form', this.chapters);
-    if (this.chapters.value == null) {
+
+    if (this.chapters.value == null || this.chapters.value.length < 1) {
       return this.uvachaList;
-    } else {
-      let selectedChapterUvachaList:String[] = [];
-      for (let chapter of this.chapters.value) {
-        this.gita_Json.forEach((row) => {
-          if (row.chapter_number == chapter.split(' ')[1]) {
-            selectedChapterUvachaList.push(row['uvacha']);
-          }
-        });
-      }
-      return [...new Set(selectedChapterUvachaList)];
     }
+
+    let chapterFilteredSlokas: any[] = this.getChapterFilteredSlokas();
+    let selectedChapterUvachaList: String[] = [];
+    chapterFilteredSlokas.forEach(sloka => selectedChapterUvachaList.push(sloka['uvacha']))
+    return [...new Set(selectedChapterUvachaList)];
   }
 
   oneWordClick(event: any) {
@@ -85,11 +80,9 @@ export class WordcloudComponent implements OnInit {
   }
 
   assignWordMinMaxSize(wordObjectArray: any[]) {
-    let countArray:String[] = [];
-    wordObjectArray.forEach((wordObject) => countArray.push(wordObject.count));
-    let countArrayMin: number = Number(countArray[countArray.length - 1]);
-    let countArrayMax: number = Number(countArray[0]);
-    let scaleToMin: number = 10;
+    let countArrayMin: number = Number(wordObjectArray[wordObjectArray.length - 1]['count']);
+    let countArrayMax: number = Number(wordObjectArray[0]['count']);
+    let scaleToMin: number = (countArrayMax - countArrayMin == 0)?30:10;
     let scaleToMax:number = 100;
     let multiplyRatio: number =
       (scaleToMax - scaleToMin) /
@@ -97,7 +90,7 @@ export class WordcloudComponent implements OnInit {
 
     console.log(
       'Values: ',
-      countArray,
+      wordObjectArray,
       countArrayMin,
       countArrayMax,
       multiplyRatio
@@ -112,117 +105,55 @@ export class WordcloudComponent implements OnInit {
     return wordObjectArray.slice(0);
   }
 
-  fetchD3DataArray(filteredSlokas: any) {
-    let wordArray:string[] = [];
-    for (let row of filteredSlokas) {
-      row.words.forEach((x) => wordArray.push(x));
-    }
-    let counts = {};
-    for (let word of wordArray) {
-      counts[word] = counts[word] ? counts[word] + 1 : 1;
-    }
+  saveContext() {
+    let context = {};
+    context['chapters'] = this.chapters;
+    context['uvacha'] = this.uvacha;
+    context['searchString'] = '';
+    this.commServ.setWordCloudContext(context);
+    console.log('context saved : ', context);
+  }
+  getChapterFilteredSlokas() {
+    let chapterFilteredSloka: any[] = this.gita_Json;
 
-    let word2Array:any[] = [];
-    Object.keys(counts).forEach((key) =>
-      word2Array.push({ text: key, count: counts[key] })
+    console.log("Chapters : ", this.chapters.value);
+    console.log("Uvacha : ", this.uvacha.value);
+
+    if (this.chapters.value != null && this.chapters.value.length > 0) {
+      let chapterNumber: Number[] = [];
+      this.chapters.value.forEach(x => chapterNumber.push(parseInt(x.split(' ')[1])))
+      console.log("Chapter Selected : ", chapterNumber)
+      chapterFilteredSloka = chapterFilteredSloka.filter(row => chapterNumber.includes(row['chapter_number']))
+    }
+    console.log("chapterFilteredSloka : ", chapterFilteredSloka);
+    return chapterFilteredSloka;
+  }
+  reloadData() {
+    this.saveContext();
+
+    let chapterFilteredSloka:any[] = this.getChapterFilteredSlokas();    
+
+    if (this.uvacha.value != null && this.uvacha.value.length > 0) {
+      chapterFilteredSloka = chapterFilteredSloka.filter(row => this.uvacha.value.includes(row['uvacha']))
+    }
+    console.log("Final chapterFilteredSloka : ", chapterFilteredSloka);
+
+    let wordCountDict: any = {};
+    chapterFilteredSloka.map(row => {
+      row.words.forEach((word:string) => {
+          wordCountDict[word] = wordCountDict[word] ? wordCountDict[word] + 1 : 1;
+      })
+    })
+
+    let word2Array: any[] = [];
+    Object.keys(wordCountDict).forEach((key) =>
+      word2Array.push({ text: key, count: wordCountDict[key] })
     );
     word2Array.sort((a, b) => (a.count < b.count ? 1 : -1));
 
-    this.data = this.assignWordMinMaxSize(word2Array);
+    this.data = this.assignWordMinMaxSize(word2Array.slice(0,1000));
+
   }
+  
 
-  reloadData(event: any) {
-    console.log('Uvacha Changed', event);
-    let filteredSlokas:any[] = [];
-    for (let row of this.gita_Json) {
-      if (this.chapters.value != null && this.chapters.value.length > 0) {
-        for (let chapter of this.chapters.value) {
-          if (this.uvacha.value != null && this.uvacha.value.length > 0) {
-            for (let uvac of this.uvacha.value) {
-              //console.log(chapter, uvac, row.uvacha, row.chapter_number);
-              if (
-                row.uvacha == uvac &&
-                row.chapter_number == chapter.split(' ')[1]
-              ) {
-                filteredSlokas.push(row);
-              }
-            }
-          } else {
-            //all uvacha
-            if (row.chapter_number == chapter.split(' ')[1]) {
-              filteredSlokas.push(row);
-            }
-          }
-        }
-      } else {
-        //all chapters
-        //console.log('Row in reload data : ', row);
-
-        if (this.uvacha.value != null && this.uvacha.value.length > 0) {
-          for (let uvac of this.uvacha.value) {
-            if (row.uvacha == uvac) {
-              filteredSlokas.push(row);
-            }
-          }
-        } else {
-          filteredSlokas = this.gita_Json;
-          break;
-        }
-      }
-    }
-    console.log('Reloaded Data : ', filteredSlokas);
-    this.fetchD3DataArray(filteredSlokas);
-  }
-
-
-  d3Script() {
-    /* <script>
-
-    // List of words
-    var myWords = [{ word: "Running", size: "10" }, { word: "Surfing", size: "20" }, { word: "Climbing", size: "50" }, { word: "Kiting", size: "30" }, { word: "Sailing", size: "20" }, { word: "Snowboarding", size: "60" }]
-
-    // set the dimensions and margins of the graph
-    var margin = { top: 10, right: 10, bottom: 10, left: 10 },
-      width = 450 - margin.left - margin.right,
-      height = 450 - margin.top - margin.bottom;
-
-    // append the svg object to the body of the page
-    var svg = d3.select("#my_dataviz").append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-      .attr("transform",
-        "translate(" + margin.left + "," + margin.top + ")");
-
-    // Constructs a new cloud layout instance. It run an algorithm to find the position of words that suits your requirements
-    // Wordcloud features that are different from one word to the other must be here
-    var layout = d3.layout.cloud()
-      .size([width, height])
-      .words(myWords.map(function (d) { return { text: d.word, size: d.size }; }))
-      .padding(5)        //space between words
-      .rotate(function () { return ~~(Math.random() * 2) * 90; })
-      .fontSize(function (d) { return d.size; })      // font size of words
-      .on("end", draw);
-    layout.start();
-
-    // This function takes the output of 'layout' above and draw the words
-    // Wordcloud features that are THE SAME from one word to the other can be here
-    function draw(words) {
-      svg
-        .append("g")
-        .attr("transform", "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")")
-        .selectAll("text")
-        .data(words)
-        .enter().append("text")
-        .style("font-size", function (d) { return d.size; })
-        .style("fill", "#69b3a2")
-        .attr("text-anchor", "middle")
-        .style("font-family", "Impact")
-        .attr("transform", function (d) {
-          return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
-        })
-        .text(function (d) { return d.text; });
-    }
-  </script> */
-  }
 }
