1b05ecad45fa5584588748ba80db29fb26648f5c
Padmaraj Bhat <padmarajbhat@gmail.com>
2022-07-05 10:54:21 +0530

    word highlight on partial word match for devanagari script and its build

--- AngularFrontEnd/src/app/Components/search-result/search-result.component.ts
+++ AngularFrontEnd/src/app/Components/search-result/search-result.component.ts
@@ -37,4 +37,78 @@ export class SearchResultComponent implements OnInit {
     this.commServ.setSlokaContext(context);
     this.commServ.switchTo(2);
   }
+
+  getSlokaHighlightWords(sloka: String) {
+    if (sloka.includes('ज्ञानमावृत्य') ){
+      console.log("What is in : ", sloka)
+    }
+    let lines = sloka.split('।').slice(0, 2);
+
+    let startIndex = sloka.indexOf(this.commServ.getSearchText());
+    let endIndex = startIndex + this.commServ.getSearchText().length;
+    //console.log("Indexes : ", startIndex, endIndex, lines);
+
+    let assignIndex = 0;
+    let wordStructArray: any[] = [];
+
+    lines[0].split(" ").forEach(word => {
+      word = word.replaceAll('\n', '').replaceAll(' ', '')
+      wordStructArray.push(
+        {
+          'word': word,
+          /* 
+           * */
+          'highlight':
+            //word is overlapping searched word
+            ((startIndex <= assignIndex) && (assignIndex < endIndex))
+              || ((startIndex <= assignIndex + word.length) && (assignIndex + word.length < endIndex))
+
+              //word is smaller than searched word
+              || ((startIndex <= assignIndex) && (assignIndex + word.length < endIndex))
+
+              //word is bigger than searched word
+              || ((assignIndex <= startIndex) && (endIndex <= assignIndex + word.length)) ? true : false,
+
+          'indexStart': assignIndex,
+          'indexEnd': assignIndex + word.length
+        })
+      if (word == 'ज्ञानमावृत्य') {
+        console.log(word, startIndex, assignIndex, assignIndex + word.length + 1, endIndex)
+      }
+      assignIndex += word.length + 1;
+      //console.log(word, startIndex, assignIndex,  assignIndex + word.length + 1, endIndex)
+    })
+
+    let wordStructArray2: any[] = [];
+
+    //+1 for '|'
+    //assignIndex += 1
+
+    lines[1].split(" ").forEach(word => {
+      word = word.replaceAll('\n', '').replaceAll(' ', '')
+
+      wordStructArray2.push(
+        {
+          'word': word,
+          'highlight': //word is overlapping searched word
+            ((startIndex <= assignIndex) && (assignIndex < endIndex))
+              || ((startIndex <= assignIndex + word.length) && (assignIndex + word.length < endIndex))
+
+              //word is smaller than searched word
+              || ((startIndex <= assignIndex) && (assignIndex + word.length < endIndex))
+
+              //word is bigger than searched word
+              || ((assignIndex <= startIndex) && (endIndex <= assignIndex + word.length)) ? true : false,
+          'indexStart': assignIndex,
+          'indexEnd': assignIndex + word.length
+        })
+      if (word == 'ज्ञानमावृत्य') {
+        console.log(word, startIndex, assignIndex, assignIndex + word.length + 1, endIndex)
+      }
+      assignIndex += word.length + 1;
+    })
+
+    //console.log("Display Array : ", [wordStructArray, wordStructArray2])
+    return [wordStructArray, wordStructArray2]
+  }
 }
